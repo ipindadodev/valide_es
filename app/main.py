d659101cd50d2ce2d4ed2c9dbd43435be6a79315
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-04-14 16:15:04 +0200

    Refactor importaciones y registro de routers en la API Valide.es

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
-from app.api import nif
+from app.api.nif import router as nif_router
+from app.api.iban import router as iban_router
+# from app.api.telefono import router as telefono_router
+# from app.api.cp import router as cp_router
+
+
 
 app = FastAPI(
     title="Valide.es API",
@@ -7,11 +12,14 @@ app = FastAPI(
     version="1.0.0",
 )
 
-app.include_router(nif.router, prefix="/nif", tags=["NIF/DNI/NIE"])
-app.include_router(nif.router, prefix="/iban", tags=["IBAN"])
-app.include_router(nif.router, prefix="/telefono", tags=["Teléfono"])
-app.include_router(nif.router, prefix="/cp", tags=["Código postal"])
+# Routers registrados correctamente
+app.include_router(nif_router, prefix="/nif", tags=["NIF/DNI/NIE"])
+app.include_router(iban_router, prefix="/iban", tags=["IBAN"])
+# app.include_router(telefono_router, prefix="/telefono", tags=["Teléfono"])
+# app.include_router(cp_router, prefix="/cp", tags=["Código postal"])
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Valide.es API! Check the documentation at /docs"}
-    
+    return {
+        "message": "Bienvenido a la API de Valide.es. Accede a la documentación interactiva en /docs"
+    }
